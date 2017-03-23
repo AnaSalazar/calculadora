fc9708d01c949bbc0959e9054a6064ea530f884b
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-03-23 16:58:06 -0600

    primer intento de calculadora

---
+++ assets/js/calculadora.js
@@ -0,0 +1,52 @@
+function mostrar(valor) {
+  var entrada = document.getElementById("valor");
+  entrada.value += valor
+}
+
+function borrar() {
+  var borrar = document.getElementById("valor");
+  borrar.value = "";
+}
+function almacenar() {
+  var numeros = document.getElementById("valor").value;
+  var almacen = valor.split();
+  return almacen;
+}
+
+/*Opcional
+function negativos() {
+  var entrada = document.getElementById("valor");
+  var signo = document.getElementById("signo");
+  var digito = entrada.value;
+  signo += digito;
+}*/
+
+function porcentaje(){
+//opcional
+}
+
+function division() {
+
+}
+
+function digito() {
+
+}
+
+function multiplicacion() {
+
+}
+
+function resta() {
+
+}
+
+function suma(almacen,almacen) {
+  var entrada = document.getElementById("valor");
+  var igualA = (almacen[0] + almacen[1]);
+  return entrada.value = igualA;
+}
+
+function resultado() {
+
+}
